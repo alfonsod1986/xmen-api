ca5ed61e8ed01ae59a77d54852b341897c50fd04
Alfonso Delgado <poncho@MacBook-Air-de-Alfonso.local>
2021-03-22 03:28:26 -0600

    Sin conexion a mongo

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const Config = require('./conf');
-const init = require('./middlewares');
-const dbConnection = require('./db/mongo');
+const Config = require('./src/conf');
+const init = require('./src/middlewares');
+const dbConnection = require('./src/db/mongo');
 
 const startServer = async ({ port, ip, databases }) => {
     const { mongo } = databases;
     const app = await init();
 
-    //await dbConnection(mongo);
+    await dbConnection(mongo);
     
     app.listen(port, ip, err => {
         if (err) {
